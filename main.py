fd916b58456e1575fae299ae42e3f46d71ad1532
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2021-12-06 18:03:34 +0400

    Демонстрация сериализации в файл и десериализации из файла

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from Bucket_sort import Bucket_Sort
+
+filename_txt = "res.txt"
+filename_yaml = "result.yaml"
+file = Bucket_Sort()
+data = file.read_json_txt(filename_txt)
+parameters: list[str] = ["age", "passport_number"]
+file.bucket_sort(data, parameters[0])
+file.write_yaml(filename_yaml, data)
+tmp = file.read_yaml(filename_yaml)
+file.print_collection(tmp, 15)
