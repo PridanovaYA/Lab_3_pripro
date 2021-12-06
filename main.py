a5f344f6ecb5f9ac4c9d94a8711817ba5a7e6718
PridanovaYA <94101848+PridanovaYA@users.noreply.github.com>
2021-12-06 19:20:18 +0400

    Реализация готовая

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from typing import List
 from Bucket_sort import Bucket_Sort
 
 filename_txt = "res.txt"
-filename_yaml = "result.yaml"
+filename_yaml = "res.yaml"
 file = Bucket_Sort()
 data = file.read_json_txt(filename_txt)
 parameters: list[str] = ["age", "passport_number"]
-file.bucket_sort(data, parameters[0])
+data = file.bucket_sort(data, parameters[0])
 file.write_yaml(filename_yaml, data)
 tmp = file.read_yaml(filename_yaml)
-file.print_collection(tmp, 15)
+file.print_collection(tmp, 10)
